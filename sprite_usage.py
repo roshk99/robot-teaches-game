80e1f7a3973255d82355a1a0062c4c8b0b1899cb
emilykim1 <ekim2@andrew.cmu.edu>
2020-11-04 21:58:07 +0900

    robot update

--- sprite_usage.py
+++ sprite_usage.py
@@ -21,13 +21,14 @@ surface = pygame.display.set_mode((100,100))
 FPS = 120
 frames = FPS / 12
 strips = [
-    SpriteStripAnim('Explode1.bmp', (0,0,24,24), 8, 1, True, frames),
-    SpriteStripAnim('Explode2.bmp', (0,0,12,12), 7, 1, True, frames),
-    SpriteStripAnim('Explode3.bmp', (0,0,48,48), 4, 1, True, frames) +
-    SpriteStripAnim('Explode3.bmp', (0,48,48,48), 4, 1, True, frames),
-    SpriteStripAnim('Explode4.bmp', (0,0,24,24), 6, 1, True, frames),
-    SpriteStripAnim('Explode5.bmp', (0,0,48,48), 4, 1, True, frames) +
-    SpriteStripAnim('Explode5.bmp', (0,48,48,48), 4, 1, True, frames),
+    SpriteStripAnim('spritesheet_robot.png', (0,0,51,62), 8, 1, True, frames)
+    # SpriteStripAnim('Explode1.bmp', (0,0,24,24), 8, 1, True, frames),
+    # SpriteStripAnim('Explode2.bmp', (0,0,12,12), 7, 1, True, frames),
+    # SpriteStripAnim('Explode3.bmp', (0,0,48,48), 4, 1, True, frames) +
+    # SpriteStripAnim('Explode3.bmp', (0,48,48,48), 4, 1, True, frames),
+    # SpriteStripAnim('Explode4.bmp', (0,0,24,24), 6, 1, True, frames),
+    # SpriteStripAnim('Explode5.bmp', (0,0,48,48), 4, 1, True, frames) +
+    # SpriteStripAnim('Explode5.bmp', (0,48,48,48), 4, 1, True, frames),
 ]
 black = Color('black')
 clock = pygame.time.Clock()
