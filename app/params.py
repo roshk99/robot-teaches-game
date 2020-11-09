17c3aad31052f5606d8a19dc20731c63d4db0385
roshk99 <roshk99@users.noreply.github.com>
2020-11-09 14:32:02 -0600

    Updated the rule format
    - Changed the rule format to allow for multiple categories within each bin
    - Updated the rule evaluation in params
    - Updated the to and from string methods

--- app/params.py
+++ app/params.py
@@ -8,31 +8,44 @@ for color in ['red', 'green', 'purple']:
                 CARD_PROPERTIES.append((color, shading, shape, number))
 
 NUM_BINS = 2
-RULES = [[['red', 'green', 'purple'], ['open', 'striped', 'solid'],
-          ['diamond', 'oval', 'squiggle'], ['one']],
-         [['red', 'green', 'purple'], ['open', 'striped', 'solid'],
-          ['diamond', 'oval', 'squiggle'], ['two', 'three']]]
+RULES = [
+            [
+                [['red', 'green', 'purple'], ['open', 'striped', 'solid'], ['oval', 'squiggle'], ['one']]
+            ],
+            [
+                [['red', 'green', 'purple'], ['open', 'striped', 'solid'], ['diamond', 'oval', 'squiggle'], ['two', 'three']],
+                [['red', 'green', 'purple'], ['open', 'striped', 'solid'], ['diamond'], ['one']]
+            ]
+        ]
 
 CARD_ORDER = [3, 5, 6, 8, 10, 12, 34]
 NUM_TRIALS = len(CARD_ORDER)
 ANSWER = []
 for card in CARD_ORDER:
-    bin_res = [1] * NUM_BINS
-    for cur_bin in range(NUM_BINS):
-        for prop in range(4):
-            if not (CARD_PROPERTIES[card][prop] in RULES[cur_bin][prop]):
-                bin_res[cur_bin] = 0
-                break
+    bin_res = [0] * NUM_BINS
+    for cur_bin_num, cur_bin in enumerate(RULES):
+        for rule in cur_bin:
+            if bin_res[cur_bin_num] == 0:
+                res = 1
+                for prop_num, prop in enumerate(rule):
+                    if not (CARD_PROPERTIES[card][prop_num] in prop):
+                        res = 0
+                if res == 1:
+                    bin_res[cur_bin_num] = 1
     ANSWER.append(bin_res)
 
 CARD_ORDER_DEMO = [10, 12, 34]
 NUM_DEMOS = len(CARD_ORDER_DEMO)
 ANSWER_DEMO = []
 for card in CARD_ORDER_DEMO:
-    bin_res = [1] * NUM_BINS
-    for cur_bin in range(NUM_BINS):
-        for prop in range(4):
-            if not (CARD_PROPERTIES[card][prop] in RULES[cur_bin][prop]):
-                bin_res[cur_bin] = 0
-                break
+    bin_res = [0] * NUM_BINS
+    for cur_bin_num, cur_bin in enumerate(RULES):
+        for rule in cur_bin:
+            if bin_res[cur_bin_num] == 0:
+                res = 1
+                for prop_num, prop in enumerate(rule):
+                    if not (CARD_PROPERTIES[card][prop_num] in prop):
+                        res = 0
+                if res == 1:
+                    bin_res[cur_bin_num] = 1
     ANSWER_DEMO.append(bin_res)
--- utils.py
+++ utils.py
@@ -2,10 +2,12 @@ from time import strftime
 
 def rules_to_str(rules):
     rule_str = ""
-    for bin in range(len(rules)):
-        rule_str += "bin:".format(bin)
-        for prop_ind, prop_vals in enumerate(rules[bin]):
-            rule_str += "prop-{}".format(prop_vals)
+    for bin in rules:
+        rule_str += "bin:"
+        for rule in bin:
+            rule_str += "rule;"
+            for prop_ind, prop_vals in enumerate(rule):
+                rule_str += "prop-{}".format(prop_vals)
     return (rule_str)
 
 
@@ -13,12 +15,15 @@ def str_to_rules(rule_str):
     bin_strs = rule_str.split("bin:")
     rules = []
     for bin_ind, bin_str in enumerate(bin_strs[1:]):
-        prop_strs = bin_str.split("prop-")
+        rule_strs = bin_str.split("rule;")
         rules.append([])
-        for prop_str in prop_strs[1:]:
-            tmp_strs = prop_str.replace('[', '').replace(']',
-                                                         '').replace("'", '')
-            rules[bin_ind].append(tmp_strs.split(', '))
+        for rule_ind, rule_str in enumerate(rule_strs[1:]):
+            prop_strs = rule_str.split("prop-")
+            rules[bin_ind].append([])
+            for prop_str in prop_strs[1:]:
+                tmp_strs = prop_str.replace('[', '').replace(']',
+                                                            '').replace("'", '')
+                rules[bin_ind][rule_ind].append(tmp_strs.split(', '))
     return rules
 
 def get_user_index(id, time):
